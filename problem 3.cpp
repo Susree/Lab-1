1f3cd42d5ac104fc408bc983d66ebc200a83660c
Susree <31284951+Susree@users.noreply.github.com>
2017-08-25 12:52:15 +0530

    Create problem 3.cpp

---
+++ problem 3.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+ using namespace std;
+ int main () 
+ {
+ 	int a,b,s,d,p,q;
+ 	cout << "enter two numbers"<< endl;
+ 	cin >> a;
+ 	cin >> b;
+ 	s=a+b;
+ 	d=a-b;
+ 	p=a*b;
+ 	q=a/b;
+ 	cout << "sum="<<s << endl;
+ 	cout <<"difference="<<d << endl;
+ 	cout <<"product="<<p << endl;
+ 	cout <<"division="<<q;
+ 	
+ 
+ 	return 0;
+ }
