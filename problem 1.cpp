4846a8157f5e566cd420f0908bde5329d3c853e0
Susree <31284951+Susree@users.noreply.github.com>
2017-08-25 12:08:43 +0530

    Create problem 1.cpp

---
+++ problem 1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int a;
+	float b;
+	char c;
+	bool t=true;
+	bool f=false;
+	cout << "enter an integer"<< endl;
+	cin >> a;
+	cout << "Integer=" << a << endl;
+	cout << "enter a float"<< endl;
+	cin >> b;
+	cout << "Float=" << b << endl;
+	cout << "enter a character"<< endl;
+	cin >> c;
+	cout << "character=" << c << endl;
+	cout << t << endl;
+	cout << f;
+	
+	return 0;
+}
