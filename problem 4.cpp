1ce1b5c4e608e67fd5817899b72b6af78deb1d24
Susree <31284951+Susree@users.noreply.github.com>
2017-08-25 12:23:09 +0530

    Create problem 4.cpp

---
+++ problem 4.cpp
@@ -0,0 +1,13 @@
+ #include<iostream>
+ using namespace std;
+ int main () 
+ {
+ 	int l,b,p;
+ 	cout << "enter a length"<< endl;
+ 	cin >> l;
+ 	cout << "enter a breadth"<<  endl;
+ 	cin >> b;
+ 	p=2*(l+b);
+ 	cout << "perimeter ="<< p;
+ 	return 0;
+ }
