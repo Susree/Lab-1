1a5d84062bb2f3704cfcdf773a5b248cb4ecaddf
Susree <31284951+Susree@users.noreply.github.com>
2017-08-25 12:27:37 +0530

    Create problem 5.cpp

---
+++ problem 5.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+ using namespace std;
+ int main () 
+ {
+ 	int l,b,a;
+ 	cout << "enter a length"<< endl;
+ 	cin >> l;
+ 	cout << "enter a breadth"<< endl;
+ 	cin >> b;
+ 	a=l*b;
+ 	cout << "area=" <<a;
+ 	return 0;
+ }
