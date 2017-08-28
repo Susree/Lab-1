5e576a83ae820ee9fec1ae463e8c57925458bdc0
Susree <31284951+Susree@users.noreply.github.com>
2017-08-28 16:19:05 +0530

    Create problem 11.cpp

---
+++ problem 11.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() 
+{
+float a,b,c;
+cout <<"enter 2 numbers"<<endl;
+cin >>a;
+cin >>b;
+c=pow(a,b);
+cout <<" result="<<c<<endl;
+
+return 0;
+}
+
