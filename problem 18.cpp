e0df08dff88f3561a706c070821ef2f955feac47
Susree <31284951+Susree@users.noreply.github.com>
2017-08-28 17:03:39 +0530

    Create problem 18.cpp

---
+++ problem 18.cpp
@@ -0,0 +1,16 @@
+int main() 
+{
+float a,b,c,d;
+cout <<"enter a principal"<<endl;
+cout <<"enter a rate of interest";
+cout<<"enter a time";
+cin >>a;
+cin >>b;
+cin >>c;
+d=pow(a*(100+b)/100,c)
+;
+cout <<"compound interest="<<d<<endl;
+
+return 0;
+}
+
