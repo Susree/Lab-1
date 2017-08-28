aa15103e833b5894f5ec6a7889e6482d55d77dd6
Susree <31284951+Susree@users.noreply.github.com>
2017-08-28 15:14:16 +0530

    Create problem 8.cpp

---
+++ problem 8.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+float c,f;
+cout <<" enter a temprature in c "<<endl;
+cin >>f ;
+
+f=9*c/5+32;
+cout <<"equivalent distance in fahrenhiet="<<f<<endl;
+return 0;
+}
+
