db9c16c0cd71c5cedcfe894671a08f3d5f437403
Susree <31284951+Susree@users.noreply.github.com>
2017-08-28 16:22:26 +0530

    Create problem 12.cpp

---
+++ problem 12.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() 
+{
+float a,b;
+cout <<"enter a number"<<endl;
+cin >>a;
+
+b=pow(a,0.5);
+cout <<" result="<<b<<endl;
+
+return 0;
+}
+
