7ccd92898f951ebe82d96576397bc49a051c028b
Susree <31284951+Susree@users.noreply.github.com>
2017-08-28 16:38:35 +0530

    Create problem 14.cpp

---
+++ problem 14.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+float a,b,c;
+cout <<"enter a base"<<endl;
+cin >>a;
+cout <<"enter a height";
+cin >>b;
+
+c=(1*a*b)/2;
+cout <<" area="<<c<<endl;
+
+return 0;
+}
+
