f09d667ae93d0057c7940a64aeb1f38c71eb3b5f
Susree <31284951+Susree@users.noreply.github.com>
2017-08-28 15:06:41 +0530

    Create problem 7.cpp

---
+++ problem 7.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+float cm,m,km;
+cout <<" enter a distance in cm "<<endl;
+cin >> m;
+cin >> km;
+m=cm*100;
+km=cm*100000;
+cout <<"equivalent distance in metres="<<m<<endl;
+cout <<"equivalent distance in kilometres="<<km<<endl;
+
+	
+	return 0;
+}
