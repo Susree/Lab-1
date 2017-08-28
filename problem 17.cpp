073c0428b77934d024558115a414f91e9a0d9c17
Susree <31284951+Susree@users.noreply.github.com>
2017-08-28 15:45:04 +0530

    Create problem 17.cpp

---
+++ problem 17.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+float a,b,c,d,e,t,m,p;
+cin >>a;
+cin >>b;
+cin >>c;
+cin >> d;
+cin >>e;
+t=a+b+c+d+e;
+m=t/5;
+p=m*100;
+cout <<" total="<<t<<endl;
+cout <<"average="<<m<<endl;
+cout <<"percentage="<<p<<endl;
+
+return 0;
+}
+
