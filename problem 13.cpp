c4d0e4f25d50971e6533a5c135791b05a53a6588
Susree <31284951+Susree@users.noreply.github.com>
2017-08-28 16:26:18 +0530

    Create problem 13.cpp

---
+++ problem 13.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+float a,b,c;
+cout <<"enter 2 numbers"<<endl;
+cin >>a;
+cin >>b;
+cin >>c;
+
+c=180-(a+b);
+cout <<" result="<<c<<endl;
+
+return 0;
+}
+
