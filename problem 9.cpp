2872b11c3a841aec315ec639ea377eee746b5843
Susree <31284951+Susree@users.noreply.github.com>
2017-08-28 15:25:09 +0530

    Create problem 9.cpp

---
+++ problem 9.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+float c,f;
+cout <<" enter a temprature in f"<<endl;
+cin >>c;
+
+c=5*(f-32)/9;
+cout <<"equivalent temperature in celcius="<<c<<endl;
+return 0;
+}
+
