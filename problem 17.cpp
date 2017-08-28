9ebab57e8b6680cb5095a69555443a1262f32a00
Susree <31284951+Susree@users.noreply.github.com>
2017-08-28 16:53:04 +0530

    Create problem 17.cpp

---
+++ problem 17.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+float a,b,c,d;
+cout <<"enter 3 numbers"<<endl;
+cin >>a;
+cin >>b;
+cin >>c;
+d=(a*b*c)/100;
+cout <<"simple interest="<<d<<endl;
+
+return 0;
+}
+
